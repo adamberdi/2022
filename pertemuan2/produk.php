861dbf6b4dd7cd65392a18997ea26ce4e3f5fb6e
adamberdi <adamberdi46@gmail.com>
2022-01-11 02:09:59 +0700

    Menyelesaikan video pertemuan 2

--- pertemuan2/produk.php
+++ pertemuan2/produk.php
@@ -1,4 +1,13 @@
 <?php
+/*
+Adam Berdi RA
+203040180
+https://github.com/adamberdi/prakweb2021_oophp_203040180
+Pertemuan 2 - 11 Januari 2022
+Mempelajari object pada PHP
+*/
+?>
+<?php
 
 // Jualan Produk 
 // Komik
